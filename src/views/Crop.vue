01bef00702d1e74534b02dda3ae0d42868b2f145
Roman Strakhov <roman.strakhov@alberblanc.com>
2024-07-22 23:34:58 +0200

    wip

--- src/views/Crop.vue
+++ src/views/Crop.vue
@@ -2,32 +2,39 @@
 import {store} from "@/store/store";
 import {onMounted, ref} from "vue";
 import router from "@/router";
-const imgElement = ref(null)
 import VueCropper from 'vue-cropperjs';
 import 'cropperjs/dist/cropper.css';
 
 let cropper = null
 
 onMounted(() => {
-  if (store.state.originalImage === null) {
-    router.push('/')
-    return
-  }
-  // imgElement.value.addEventListener('load')
-  // imgElement.value.src = store.state.originalImage.src
+  if (store.state.originalImage === null) { router.push('/') }
 })
+
+const onCropFinished = () => {
+  // TODO save cropped to store; check coins etc.
+  router.push('method')
+}
 
 </script>
 
 <template>
-  <div>
-    Crop image:
+  <div class="flex justify-between items-center">
+    <button class="btn" @click="router.push('/')">
+      Back
+    </button>
+    <div>
+      Crop image
+    </div>
+    <button class="btn mx-2" @click="onCropFinished">
+      Next
+    </button>
   </div>
   <div class="flex">
-        <vue-cropper
-            class="w-full h-80vh"
-            ref="cropper"
-            :src="store.state.originalImage?.src ?? ''"/>
+    <VueCropper
+        class="w-full h-80vh"
+        ref="cropper"
+        :src="store.state.originalImage?.src ?? ''"/>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import UploadOrChoose from "@/views/UploadOrChoose.vue";
-</script>
-
-<template>
-  <div class="home">
-    <UploadOrChoose/>
-  </div>
-</template>
