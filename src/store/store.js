a2dac6788754f8afe4f08781b4f64c071d9e8e1f
Roman Strakhov <roman.strakhov@alberblanc.com>
2024-07-22 10:56:43 +0200

    wip

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,16 @@ import { createStore } from 'vuex'
 
 // Create a new store instance.
 export const store = createStore({
+    strict: true,
     state () {
         return {
             coins: 9,
         }
     },
     getters: {
+        twiceCoins (state) {
+            return state.coins * 2
+        }
     },
     mutations: {
         decrementCoins (state) {
