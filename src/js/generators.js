faae981ae5c982f5b76e41934009e78c975096f8
Roman Strakhov <roman.strakhov@alberblanc.com>
2025-02-22 14:10:16 +0400

    wip

--- src/js/generators.js
+++ src/js/generators.js
@@ -17,21 +17,36 @@ export const generatePermutedGrid = (isFirstVariation = -1) => {
 export const generateConstantGrid = () => {
     return [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 }
-export const generateSetOfGrids = (numFeatures) => {
+export const generateSetOfGridsMaximumDifficulty = (numFeatures) => {
     switch (numFeatures) {
         case 1:
             return [generatePermutedGrid()];
         case 2:
             return [generatePermutedGrid(), generateAlignedGrid()];
         case 3:
-            return [generatePermutedGrid(), generateAlignedGrid(0), generateAlignedGrid(1)];
-        case 4:
-            return [generatePermutedGrid(), generateAlignedGrid(0), generateAlignedGrid(1), generateConstantGrid()];
-        case 5:
-            return [generatePermutedGrid(0), generatePermutedGrid(1), generateAlignedGrid(0), generateAlignedGrid(1), generateConstantGrid()];
+            return Math.random() > 0.5
+                ? [generatePermutedGrid(),  generateAlignedGrid(0),  generateAlignedGrid(1)]
+                : [generatePermutedGrid(0), generatePermutedGrid(1), generateAlignedGrid()]
+        case 4: {
+            if (Math.random() > 0.8) {
+                return [generatePermutedGrid(0), generatePermutedGrid(0), generatePermutedGrid(1), generateAlignedGrid()]
+            }
+            if (Math.random() > 0.7) {
+                return [generatePermutedGrid(0), generatePermutedGrid(1), generatePermutedGrid(1), generateAlignedGrid()]
+            }
+            return [generatePermutedGrid(0), generatePermutedGrid(1), generateAlignedGrid(0), generateAlignedGrid(1)]
+        }
         default:
-            console.error("Unsupported number of features", numFeatures);
+            console.error("generateSetOfGridsMaximumDifficulty: Unsupported number of features", numFeatures);
     }
+}
+
+export const generateSetOfGrids = (numFeatures, numNonConstantFeatures) => {
+    const grids = generateSetOfGridsMaximumDifficulty(numNonConstantFeatures);
+    for (let i = 0; i < numFeatures - numNonConstantFeatures; i++) {
+        grids.push(generateConstantGrid());
+    }
+    return grids;
 }
 
 const randomThreeElements = (array) => {
