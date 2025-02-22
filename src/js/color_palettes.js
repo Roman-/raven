ec864ab918bd6d98a55e2f1e9e75418d8b1578f2
Roman Strakhov <roman.strakhov@alberblanc.com>
2025-02-22 21:36:39 +0400

    wip

--- src/js/color_palettes.js
+++
@@ -1,6 +0,0 @@
-// TODO add fromhttps://colorhunt.co/palettes/light
-export const goodGradientColors = [
-    "#CCF1FF",
-    "#E0D7FF",
-    "#FFCCE1",
-]
--- src/js/draw_puzzle.js
+++ src/js/draw_puzzle.js
@@ -1,8 +1,9 @@
 // cells - 2D array of cell objects
 // drawCell - function that draws a single cell (ctx, cell, x, y, size)
 export const drawPuzzleGrid = (ctx, startX, startY, puzzleSize, cells, drawCell) => {
-    ctx.fillStyle = "#000"
-    ctx.fillRect(startX, startY, puzzleSize, puzzleSize)
+    ctx.strokeStyle = "#00000055";
+    ctx.lineWidth = 1;
+    ctx.strokeRect(startX, startY, puzzleSize, puzzleSize);
 
     const cellSize = puzzleSize / 3
 
@@ -11,6 +12,12 @@ export const drawPuzzleGrid = (ctx, startX, startY, puzzleSize, cells, drawCell)
             const cell = cells[row][col]
             const cellX = startX + col * cellSize
             const cellY = startY + row * cellSize
+
+            // outline the cell
+            ctx.strokeStyle = "#00000033";
+            ctx.lineWidth = 1;
+            ctx.strokeRect(cellX, cellY, cellSize, cellSize);
+
             ctx.save()
             drawCell(ctx, cell, cellX, cellY, cellSize)
             ctx.restore()
