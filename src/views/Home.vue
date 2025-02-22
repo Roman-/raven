3b18700ace6989cfab791aa44050fc555fb1ec69
Roman Strakhov <roman.strakhov@alberblanc.com>
2025-02-22 21:49:41 +0400

    wip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import {onMounted, ref, watch} from 'vue';
 import {drawPuzzleGrid} from "@/js/draw_puzzle";
 import {store} from "@/store/store";
 
@@ -7,15 +7,15 @@ const puzzleCanvasSize = ref(0);
 const puzzleCanvas = ref(null);
 
 onMounted(() => {
-  // min of window w/h * 0.8
+  watch( () => store.state.cellsAndAnswers, draw, { deep: true } )
   puzzleCanvasSize.value = Math.round(Math.min(window.innerWidth, window.innerHeight) * 0.8);
-  store.commit('generate');
-  setTimeout(draw, 1);
+  setTimeout(() => {store.commit('generate')}, 1);
 })
 
 const draw = () => {
-  drawPuzzleGrid(puzzleCanvas.value.getContext('2d'), 0, 0,
-      puzzleCanvasSize.value, store.getters.cells, store.getters.drawShapeFunction);
+  let ctx = puzzleCanvas.value.getContext('2d');
+  ctx.clearRect(0, 0, puzzleCanvasSize.value, puzzleCanvasSize.value);
+  drawPuzzleGrid(ctx, 0, 0, puzzleCanvasSize.value, store.getters.cells, store.getters.drawShapeFunction);
 }
 
 </script>
@@ -24,5 +24,6 @@ const draw = () => {
   <div class="flex justify-center gap-2 mt-4">
     <canvas ref="puzzleCanvas" :width="puzzleCanvasSize" :height="puzzleCanvasSize"/>
   </div>
+  <button class="btn btn-primary" @click="store.commit('generate')">Generate New Puzzle</button>
 </template>
 
