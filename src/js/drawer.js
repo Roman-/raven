0c2a6a0accbb5409f7813fdbc00d32175726ed42
Roman Strakhov <roman.strakhov@alberblanc.com>
2025-02-27 14:15:28 +0400

    wip

--- src/js/drawer.js
+++ src/js/drawer.js
@@ -41,6 +41,7 @@ export const drawPuzzleGrid = (ctx, startX, startY, puzzleSize, cells, drawCell,
 };
 
 const drawQuestionMark = (ctx, x, y, size) => {
+    ctx.fillStyle = "#aaaaaa";
     ctx.font = `${Math.floor(size * 0.6)}px Arial`;
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
